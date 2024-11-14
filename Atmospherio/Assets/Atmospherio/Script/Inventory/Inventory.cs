ad72061421bd1431237a6d84bb553c482c7cb0a7
ElectroSpeed <150343183+ElectroSpeed@users.noreply.github.com>
2024-11-14 10:01:19 +0100

    Chest pour mon lulu

--- Atmospherio/Assets/Atmospherio/Script/Inventory/Inventory.cs
+++ Atmospherio/Assets/Atmospherio/Script/Inventory/Inventory.cs
@@ -5,7 +5,68 @@ public class Inventory : MonoBehaviour
 {
     [SerializeField] GameObject _emptyItem;
     public List<InventorySlot> _slots = new List<InventorySlot>();
-    private InventoryUI _inventoryUI;
+
+    public bool AddItemInChest(Item item, int quantity, GameObject chest)
+    {
+        List<InventorySlot> chestSlots = new List<InventorySlot>();
+        for(int i = 0; i < chest.transform.childCount; i++)
+        {
+            if (chest.transform.GetChild(i).GetComponent<InventorySlot>() != null)
+            {
+                chestSlots.Add(chest.transform.GetChild(i).GetComponent<InventorySlot>());
+            }
+        }
+
+        if (chestSlots.Count == 0)
+        {
+            Debug.LogError("Pas de slots d'inventaire disponibles.");
+            return false;
+        }
+
+        List<InventorySlot> slotsEmpty = new List<InventorySlot>();
+        List<InventorySlot> slotsSeemsItem = new List<InventorySlot>();
+
+        foreach (InventorySlot slot in chestSlots)
+        {
+            if (slot._item != null && slot._item._itemName == item._itemName && !slot.IsFull())
+            {
+                slotsSeemsItem.Add(slot);
+            }
+            else if (slot.IsEmpty())
+            {
+                slotsEmpty.Add(slot);
+            }
+        }
+
+        foreach (InventorySlot slot in slotsSeemsItem)
+        {
+            ItemUI childItem = slot.transform.GetChild(0).GetComponent<ItemUI>();
+            int spaceInSlot = item._maxStack - slot._quantity;
+            int toAdd = Mathf.Min(quantity, spaceInSlot);
+            slot._quantity += toAdd;
+            quantity -= toAdd;
+            childItem.SetItem(slot);
+
+            if (quantity <= 0)
+            {
+                return true;
+            }
+        }
+        foreach (InventorySlot slot in slotsEmpty)
+        {
+            GameObject childItem = Instantiate(_emptyItem, slot.transform);
+            int toAdd = Mathf.Min(quantity, item._maxStack);
+            slot._item = item;
+            slot._quantity = toAdd;
+            quantity -= toAdd;
+            childItem.GetComponent<ItemUI>().SetItem(slot);
+
+            if (quantity <= 0)
+            {
+                return true;
+            }
+        }
+    }
 
     public void AddItem(Item item, int quantity)
     {
